ade6deb6d42b41b4fd62397a59be9a79e12df89c
Sean <seangullery@gmail.com>
2020-08-11 19:02:18 +1200

    Updated Unity

---
+++ Assets/First Person Drifter Controller/Scripts/Optional/CheckIfBelowLevel.cs
@@ -0,0 +1,42 @@
+﻿// by @torahhorse
+
+// Instructions:
+// Place on player. OnBelowLevel will get called if the player ever falls below
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class CheckIfBelowLevel : MonoBehaviour
+{
+	public float resetBelowThisY = -100f;
+	public bool fadeInOnReset = true;
+	
+	private Vector3 startingPosition;
+	
+	void Awake()
+	{
+		startingPosition = transform.position;
+	}
+	
+	void Update ()
+	{
+		if( transform.position.y < resetBelowThisY )
+		{
+			OnBelowLevel();
+		}
+	}
+	
+	private void OnBelowLevel()
+	{
+		Debug.Log("Player fell below level");
+	
+		SceneManager.LoadScene(0);
+
+
+		
+		
+		// alternatively, you could just reload the current scene using this line:
+		//Application.LoadLevel(Application.loadedLevel);
+	}
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+   
+    public GameObject[] loadout;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
