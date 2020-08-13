4b3f5f61eb7f9d072aa74c7c311f1b840ab33599
Sean <seangullery@gmail.com>
2020-08-13 15:28:40 +1200

    Remove ignored files

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,20 +1,47 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-   
-    public GameObject[] loadout;
 
+    public float damage = 10f;
+    public float range = 100f;
+    public float impactforce = 30f;
+    public Camera FpsCam;
+    public ParticleSystem Muzzleflash;
+    public GameObject impactEffect;
+    public float fireRate = 15f;
 
-    void Start()
-    {
-        
-    }
-
+    private float nextTimeToFire = 0f;
     void Update()
     {
-        
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+        }
     }
+
+    void Shoot()
+    {
+        Muzzleflash.Play();
+        RaycastHit hitinfo;
+        if (Physics.Raycast(FpsCam.transform.position, FpsCam.transform.forward, out hitinfo, range))
+        {
+            Debug.Log(hitinfo.transform.name);
+           Enemy target = hitinfo.transform.GetComponent<Enemy>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+
+            if (hitinfo.rigidbody != null)
+            {
+                hitinfo.rigidbody.AddForce(-hitinfo.normal * impactforce);
+            }
+
+            Instantiate(impactEffect, hitinfo.point, Quaternion.LookRotation(hitinfo.normal));    
+        }
+    }
+
 }
