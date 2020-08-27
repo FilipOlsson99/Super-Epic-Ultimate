e71bca93faf50773365c370e05277064ab394825
Theodor <theodorenglund@gmail.com>
2020-08-27 12:57:55 +1200

    Audio

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public Sound[] sounds;
+
+    void Awake() {
+        foreach (Sound s in sounds)
+
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.CLip;
+
+            s.source.volume = s.pitch;
+            s.source.pitch = s.pitch;
+
+        }
+    }
+
+    // Update is called once per framesdfsf
+    void Update()
+    {
+        
+    }
+}
