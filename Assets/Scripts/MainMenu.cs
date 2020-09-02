7c0d3ecdd951d258caac26a3d92cddf0e6593521
Dan <Ducatidan@outlook.com>
2020-09-02 13:43:47 +1200

    UI Screens & More

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void ClickStart()
+    {
+        Debug.Log("Scene Loaded");
+        SceneManager.LoadScene("the o");
+    }
+
+    public void ClickInstructions()
+    {
+        Debug.Log("Instructions");
+        SceneManager.LoadScene("Instructions");
+    }
+
+    public void ClickMainMenu()
+    {
+        Debug.Log("StartMenu");
+        SceneManager.LoadScene("StartScreen");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
